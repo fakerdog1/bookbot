0a99c06b00413d5139cc4f3b20ed8efb551ce619
Martin Simeonov <simeonoff.martin@gmail.com>
2025-07-31 15:26:29 +0300

    print incredibly complex dict

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import get_word_count
+from stats import get_num_count, get_word_count, get_sorted_dict
 
 def get_book_text(filepath):
     with open(filepath) as f:
@@ -7,6 +7,18 @@ def get_book_text(filepath):
     return text
         
 
-text = get_book_text('./books/frankenstein.txt')
-count = get_word_count(text)
-print(f'{count} words found in the document')
+text = get_book_text('books/frankenstein.txt')
+count = get_num_count(text)
+# print(f'{count} words found in the document')
+word_count = get_word_count(text)
+# print(word_count)
+sorted_word_count = get_sorted_dict(word_count)
+
+print('============ BOOKBOT ============')
+print('Analyzing book found at books/frankenstein.txt...')
+print('----------- Word Count ----------')
+print(f'Found {count} total words')
+print('--------- Character Count -------')
+for key, value in sorted_word_count.items():
+    if key.isalpha():
+        print(f'{key}: {value}')
--- stats.py
+++ stats.py
@@ -1,3 +1,35 @@
-def get_word_count(text):
+def get_num_count(text):
     arr = text.split()
     return len(arr)
+
+def get_word_count(text):
+    arr = text.split()
+    return_dict = {}
+    for word in text:
+        word_lower = word.lower()
+        if word_lower not in return_dict:
+            return_dict[word_lower] = 1
+            continue
+        
+        return_dict[word_lower] += 1
+    
+    return return_dict
+
+def sort_on(items):
+    return items["num"]
+
+def get_sorted_dict(dict):
+    new_dict = []
+    for key, value in dict.items():
+        new_dict.append({
+            "char": key,
+            "num": value
+        })
+
+    new_dict.sort(reverse=True, key=sort_on)
+
+    return_dict = {}
+    for item in new_dict:
+        return_dict[item["char"]] = item["num"]
+
+    return return_dict
