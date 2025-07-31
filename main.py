a2ad480e1499464de81f7806a0c58c554806793b
Martin Simeonov <simeonoff.martin@gmail.com>
2025-07-31 15:04:25 +0300

    separate a stats file for stats

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print("greetings boots")
+from stats import get_word_count
+
+def get_book_text(filepath):
+    with open(filepath) as f:
+        text = f.read()
+    
+    return text
+        
+
+text = get_book_text('./books/frankenstein.txt')
+count = get_word_count(text)
+print(f'{count} words found in the document')
---
+++ stats.py
@@ -0,0 +1,3 @@
+def get_word_count(text):
+    arr = text.split()
+    return len(arr)
